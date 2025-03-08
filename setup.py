c3e16c17e7c8d0460dc79ce08f867502153f2bf7
Girish <girish.ikhe.vu@gmail.com>
2025-03-08 16:47:10 +0530

    All Dependecies are installed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Girish',
+    author_email= 'girish.ikhe.vu@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
