291369f0344ac0c0a8806d728158ebe394a9f096
Girish <girish.ikhe.vu@gmail.com>
2025-03-08 19:20:28 +0530

    The Project is Successfully Runiing

--- app/config.py
+++ app/config.py
@@ -3,5 +3,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    GROQ_API_KEY = os.getenv(GROQ_API_KEY)
+    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
     VECTOR_DB_PATH = 'vector_db'
+    HF_TOKEN=os.getenv("HF_TOKEN")
